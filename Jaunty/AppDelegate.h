747ba0613a5e8f77f25d097cd6b03e2aac07f403
newuser <newuser@newusers-iMac.home>
2015-05-15 22:51:13 -0400

    Initial Commit

---
+++ Jaunty/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jaunty
+//
+//  Created by newuser on 5/15/15.
+//  Copyright (c) 2015 Power Streamer LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jaunty/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jaunty
+//
+//  Created by newuser on 5/15/15.
+//  Copyright (c) 2015 Power Streamer LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
